7ef2a2381c24d38266dcbbe3af6dbe3264357fbb
KlarityOS <mcr@krv.co>
2024-05-30 21:39:26 +0700

    Update clients.js

--- pages/clients.js
+++ pages/clients.js
@@ -1,1 +1,19 @@
+import React from 'react';
 
+const Clients = () => {
+  return (
+    <div>
+      <h1>Clients</h1>
+      <iframe
+        title="Power BI Dashboard"
+        width="100%"
+        height="600px"
+        src="https://app.powerbi.com/view?r=eyJrIjoiOGQ4YTUwNDctZWY3Yy00M2RmLThhNmItNjdkNGMzMDQ1ZjZjIiwidCI6IjI1OTBiMjJjLTJhNGQtNDkyNS04NjJjLWM0NTFmMzY2MTIyOSIsImMiOjEwfQ%3D%3D"
+        frameBorder="0"
+        allowFullScreen={true}
+      ></iframe>
+    </div>
+  );
+};
+
+export default Clients;

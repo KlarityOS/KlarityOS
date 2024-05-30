576b810c2e71c4206a654005768ae0b63db02f57
KlarityOS <mcr@krv.co>
2024-05-30 23:03:33 +0700

    Update clients.js

--- pages/clients.js
+++ pages/clients.js
@@ -12,7 +12,7 @@ const Clients = () => {
       <iframe
         title="Power BI Dashboard"
         width="100%"
-        height="800px"
+        height="600px"
         src="https://app.powerbi.com/view?r=eyJrIjoiOGQ4YTUwNDctZWY3Yy00M2RmLThhNmItNjdkNGMzMDQ1ZjZjIiwidCI6IjI1OTBiMjJjLTJhNGQtNDkyNS04NjJjLWM0NTFmMzY2MTIyOSIsImMiOjEwfQ%3D%3D"
         frameBorder="0"
         allowFullScreen={true}

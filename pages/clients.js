618afeebe4d23f6117d22d3ce70af6e05da5ac70
KlarityOS <mcr@krv.co>
2024-05-30 22:57:08 +0700

    Update clients.js

--- pages/clients.js
+++ pages/clients.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 const Clients = () => {
+  const headingStyle = {
+    fontSize: '78px',
+    color: 'black', 
+  };
+
   return (
     <div>
-      <h1>Clients</h1>
+      <h1 style={headingStyle}>Customers</h1> {/* Updated the text from "Clients" to "Customers" */}
       <iframe
         title="Power BI Dashboard"
         width="100%"
         height="800px"
-        src="https://app.powerbi.com/view?r=eyJrIjoiOGQ4YTUwNDctZWY3Yy00M2RmLThhNmItNjdkNGMzMDQ1ZjZjIiwidCI6IjI1OTBiMjJjLTJhNGQtNDkyNS04NjJjLWM0NTFmMzY2MTIyOSIsImMiOjEwfQ%3D%3D"
+        src="https://app.powerbi.com/view?r=eyJrIjoiOGQ4YTUwNDctZWY3Yy00M2RmLThhNmItNjdkNGMzMDQ1ZjZjIiwid"
         frameBorder="0"
         allowFullScreen={true}
       ></iframe>
